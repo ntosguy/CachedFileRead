b4b03ee42c6d7f4166b96168b24da3b1435b61dc
ntosguy <30807136+ntosguy@users.noreply.github.com>
2018-12-06 10:33:32 +0100

    Add files via upload

---
+++ Extensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace ConsoleApp1
+{
+    public static class Extensions
+    {
+        public static string CachedFileRead(string filePath)
+        {
+            string content;
+
+            string hash = filePath.ToSHA1();
+
+            if (MemoryCache.Default.Contains(hash))
+            {
+                Console.WriteLine("Cache {0} is still available in memory.", hash);
+                content = MemoryCache.Default[hash] as string;
+            }
+            else
+            {
+                Console.WriteLine("Caching {0} into memory ...", hash);
+
+                CacheItemPolicy policy = new CacheItemPolicy()
+                {
+                    AbsoluteExpiration = new DateTimeOffset(DateTime.Now.AddSeconds(8))
+                };
+
+                content = File.ReadAllText(filePath);
+				MemoryCache.Default.Set(hash, content, policy);
+            }
+
+            return content;
+        }
+
+        public static string ToSHA1(this string value)
+        {
+            using (SHA1 sha = SHA1.Create())
+            {
+                byte[] rawbytes = System.Text.Encoding.Unicode.GetBytes(value);
+                byte[] encoded = sha.ComputeHash(rawbytes);
+                return BitConverter.ToString(encoded).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Caching;
+using System.Threading;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string file in Directory.GetFiles(@"C:\Files"))
+            {
+                Extensions.CachedFileRead(file);
+            }
+
+            Console.WriteLine("Running cache reader in 3 seconds again ...");
+
+            Thread.Sleep(3000);
+
+            foreach (string file in Directory.GetFiles(@"C:\Files"))
+            {
+                Extensions.CachedFileRead(file);
+            }
+
+            Console.WriteLine("Running cache in 10 seconds again ...");
+
+            Thread.Sleep(10000);
+
+            foreach (string file in Directory.GetFiles(@"C:\Files"))
+            {
+                Extensions.CachedFileRead(file);
+            }
+
+            Console.WriteLine("Done");
+
+            Console.Read();
+        }
+    }
+}
